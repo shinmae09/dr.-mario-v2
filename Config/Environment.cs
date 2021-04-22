146af80f2a1afec9e3e44dac5195d43edd64c754
shinmae09 <46877743+shinmae09@users.noreply.github.com>
2021-04-23 00:12:46 +0800

    Add files via upload

---
+++ Config/Environment.cs
@@ -0,0 +1,8 @@
+﻿namespace DrMarioPlayer.Config
+{
+    internal class Environment
+    {
+        public static readonly int HEIGHT = 14;
+        public static readonly int WIDTH = 8;
+    }
+}
---
+++ Model/Pill.cs
@@ -0,0 +1,74 @@
+﻿using DrMarioPlayer.Converter;
+using DrMarioPlayer.Utility;
+
+namespace DrMarioPlayer.Model
+{
+    internal class Pill
+    {
+        private static readonly Position SPAWN_POSITION = new Position(3, 13);
+        private static readonly Orientation DEFAULT_ORIENTATION = Orientation.HORIZONTAL;
+
+        public Color? Color1 { get; set; }
+        public Color? Color2 { get; set; }
+        public Orientation Orientation { get; set; }
+        public Position Position { get; set; }
+
+        public Pill()
+        {
+            Orientation = DEFAULT_ORIENTATION;
+            Position = SPAWN_POSITION;
+        }
+
+        public Pill(string colorCode) : this()
+        {
+            Color1 = ColorConverter.Convert(colorCode[0]);
+            Color2 = ColorConverter.Convert(colorCode[1]);
+        }
+
+        public Pill Move(Move move)
+        {
+            Pill pill = this.Clone();
+            switch (move)
+            {
+                case Model.Move.LEFT:
+                    pill.Position.X -= 1;
+                    break;
+
+                case Model.Move.RIGHT:
+                    pill.Position.X += 1;
+                    break;
+
+                case Model.Move.DOWN:
+                    pill.Position.Y -= 1;
+                    break;
+
+                case Model.Move.ROTATE_90:
+                    pill.Orientation = OrientationHelper.Rotate(pill.Orientation, 1);
+                    break;
+
+                case Model.Move.ROTATE_180:
+                    pill.Orientation = OrientationHelper.Rotate(pill.Orientation, 2);
+                    break;
+
+                case Model.Move.ROTATE_270:
+                    pill.Orientation = OrientationHelper.Rotate(pill.Orientation, 3);
+                    break;
+            }
+
+            return pill;
+        }
+
+        public Pill Clone()
+        {
+            Pill pill = new Pill()
+            {
+                Color1 = this.Color1,
+                Color2 = this.Color2,
+                Orientation = this.Orientation,
+                Position = new Position(this.Position.X, this.Position.Y),
+            };
+
+            return pill;
+        }
+    }
+}
