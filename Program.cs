b4af1a4dc874fb2d62790c6bc18281fd52fbabd7
shinmae09 <46877743+shinmae09@users.noreply.github.com>
2021-04-23 00:16:06 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,222 @@
+﻿using DrMarioPlayer.Converter;
+using DrMarioPlayer.Model;
+using DrMarioPlayer.Utility;
+using HackathonClient;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DrMarioPlayer
+{
+    internal class Program
+    {
+        private static readonly IClient _client = new ClientService();
+        private static TcpClient socketConnection;
+        private static Tile[,] gameBoard = new Tile[Config.Environment.WIDTH, Config.Environment.HEIGHT];
+        private static Partner[,] partnerBoard = new Partner[Config.Environment.WIDTH, Config.Environment.HEIGHT];
+        private static Pill previousPill;
+        private static Pill currentPill;
+        private static Pill nextPill;
+        private static bool firstRun = true;
+        private static bool goodToGo = true;
+
+        private static async Task Main(string[] args)
+        {
+            InitializeLogger();
+
+            socketConnection = await _client.ConnectToServer();
+            if (!socketConnection.Connected)
+            {
+                Log.Information("Not connected to server.");
+            }
+            Log.Information("Connected to server.");
+
+            while (socketConnection.Connected)
+            {
+                try
+                {
+                    string message = await _client.ReceiveMessage(socketConnection);
+                    if (!String.IsNullOrEmpty(message))
+                    {
+                        goodToGo = true;
+                        Task.Run(() => UpdateGameState(message));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e.StackTrace);
+                }
+            }
+        }
+        
+        private static void UpdateGameState(string message)
+        {
+            Message arbiterMessage = JsonConvert.DeserializeObject<Message>(message);
+            var board = arbiterMessage.Coordinates;
+            currentPill = new Pill(arbiterMessage.ActivePill);
+            nextPill = new Pill(arbiterMessage.NextPill);
+
+            if ((board[3, 13] != null && board[4, 13] != null) || IsNextPill())
+            {
+                previousPill = currentPill;
+                Log.Information("Server message : " + message);
+                Parallel.For(0, gameBoard.GetLength(0), (row) =>
+                {
+                    Parallel.For(0, gameBoard.GetLength(1), (col) =>
+                    {
+                        Tile tile = TileConverter.Convert(board[row, col]);
+                        if (tile != gameBoard[row, col])
+                        {
+                            if (!IsSpawnPoint(row, col) && !firstRun)
+                            {
+                                Log.Information(gameBoard.LogGameBoard());
+                            }
+
+                            gameBoard[row, col] = tile;
+                            partnerBoard[row, col] = Partner.NONE;
+                        }
+                    });
+                });
+
+                try
+                {
+                    firstRun = false;
+                    SearchForBestMove(currentPill, nextPill);
+                }
+                catch (Exception e)
+                {
+                    Log.Information("ERROR");
+                    Log.Error(e.StackTrace);
+                }
+            }
+        }
+
+        private static void SearchForBestMove(Pill currentPill, Pill nextPill)
+        {
+            gameBoard[3, 13] = Tile.NONE;
+            gameBoard[4, 13] = Tile.NONE;
+
+            Searcher searcher = new Searcher(gameBoard);
+            searcher.Search(currentPill);
+
+            var boardScores = new ConcurrentDictionary<Tuple<Tile[,], Partner[,]>, double>();
+            var firstPillGameBoards = Simulator.LockPills(gameBoard, partnerBoard, searcher.LockedPills);
+
+            Parallel.ForEach(firstPillGameBoards.Values, (board) =>
+            {
+                Searcher nextPillSearcher = new Searcher(board.Item1);
+                nextPillSearcher.Search(nextPill.Clone());
+
+                var nextPillGameBoards = Simulator.LockPills(board.Item1, board.Item2, nextPillSearcher.LockedPills);
+                ConcurrentBag<double> scores = new ConcurrentBag<double>();
+
+                Parallel.ForEach(nextPillGameBoards.Values, (board2) =>
+                {
+                    double score = Evaluator.Evaluate(board2.Item1);
+                    scores.Add(score);
+                });
+
+                boardScores.TryAdd(board, scores.Max());
+            });
+            var bestBoard = boardScores.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+            var bestPill = firstPillGameBoards.Where(x => x.Value == bestBoard).FirstOrDefault().Key;
+
+            gameBoard = bestBoard.Item1;
+            partnerBoard = bestBoard.Item2;
+
+            List<Move> moves = searcher.GetMoveSet(bestPill.Position.X, bestPill.Position.Y, bestPill.Orientation);
+            string movesToBeSent = string.Empty;
+            var simpMoves = new List<string>();
+            for (int i = moves.Count - 1; i >= 0; i--)
+            {
+                switch (moves[i])
+                {
+                    case Move.DOWN: movesToBeSent += "D"; break;
+                    case Move.RIGHT: movesToBeSent += "R"; break;
+                    case Move.LEFT: movesToBeSent += "L"; break;
+                    case Move.ROTATE_90: movesToBeSent += "F"; break;
+                }
+
+                if (movesToBeSent.Length == 1 || i == 0)
+                {
+                    while (!goodToGo)
+                    {
+                        Task.Delay(10).GetAwaiter().GetResult();
+                    }
+
+                    int downMoves = movesToBeSent.Count(m => m == 'D');
+                    if (downMoves > 0)
+                    {
+                        goodToGo = false;
+                    }
+
+                    _client.SendMessage(socketConnection, movesToBeSent);
+                    simpMoves.Add(movesToBeSent);
+                    
+                    if (downMoves == 0)
+                    {
+                        Task.Delay(50).GetAwaiter().GetResult();
+                    }
+                    
+                    
+                    movesToBeSent = string.Empty;
+                }
+            }
+
+            if (bestPill.Orientation == Orientation.HORIZONTAL)
+            {
+                Console.WriteLine($"{Enum.GetName(typeof(Color), bestPill.Color1)} {Enum.GetName(typeof(Color), bestPill.Color2)}");
+            }
+            else if (bestPill.Orientation == Orientation.REVERSE_HORIZONTAL)
+            {
+                Console.WriteLine($"{Enum.GetName(typeof(Color), bestPill.Color2)} {Enum.GetName(typeof(Color), bestPill.Color1)}");
+            }
+            else if (bestPill.Orientation == Orientation.REVERSE_VERTICAL)
+            {
+                Console.WriteLine($"{Enum.GetName(typeof(Color), bestPill.Color1)}");
+                Console.WriteLine($"{Enum.GetName(typeof(Color), bestPill.Color2)}");
+            }
+            else
+            {
+                Console.WriteLine($"{Enum.GetName(typeof(Color), bestPill.Color2)}");
+                Console.WriteLine($"{Enum.GetName(typeof(Color), bestPill.Color1)}");
+            }
+            Console.WriteLine($"X: {bestPill.Position.X}   Y: {bestPill.Position.Y}");
+            Console.WriteLine(String.Join(" ", simpMoves));
+            Console.WriteLine("-----------------");
+        }
+
+        private static bool IsNextPill()
+        {
+            return previousPill.Color1 != currentPill.Color1 &&
+                previousPill.Color2 != currentPill.Color2;
+        }
+
+        private static bool IsSpawnPoint(int x, int y)
+        {
+            return (x == 3 && y == 13)
+                || (x == 4 && y == 13);
+        }
+        
+        private static void InitializeLogger()
+        {
+            if (File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DrMario", "log2.txt")))
+            {
+                File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DrMario", "log2.txt"));
+            }
+
+            var log = new LoggerConfiguration().WriteTo.File(
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DrMario", "log3.txt"))
+                .CreateLogger();
+
+            Log.Logger = log;
+        }
+    }
+}
---
+++ Simulator.cs
@@ -0,0 +1,278 @@
+﻿using DrMarioPlayer.Converter;
+using DrMarioPlayer.Model;
+using DrMarioPlayer.Utility;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrMarioPlayer
+{
+    internal static class Simulator
+    {
+        public static ConcurrentDictionary<Pill, Tuple<Tile[,], Partner[,]>> LockPills(Tile[,] board, Partner[,] partner, List<Pill> pills)
+        {
+            ConcurrentDictionary<Pill, Tuple<Tile[,], Partner[,]>> gameBoards = new ConcurrentDictionary<Pill, Tuple<Tile[,], Partner[,]>>();
+
+            Parallel.ForEach(pills, (pill) =>
+            {
+                Tile[,] gameBoard = (Tile[,])board.Clone();
+                Partner[,] partnerBoard = (Partner[,])partner.Clone();
+
+                Tile tile1 = TileConverter.Convert(pill.Color1.Value);
+                Tile tile2 = TileConverter.Convert(pill.Color2.Value);
+
+                switch (pill.Orientation)
+                {
+                    case Orientation.HORIZONTAL:
+                        gameBoard[pill.Position.X, pill.Position.Y] = tile1;
+                        gameBoard[pill.Position.X + 1, pill.Position.Y] = tile2;
+
+                        partnerBoard[pill.Position.X, pill.Position.Y] = Partner.RIGHT;
+                        partnerBoard[pill.Position.X + 1, pill.Position.Y] = Partner.LEFT;
+                        break;
+
+                    case Orientation.VERTICAL:
+                        gameBoard[pill.Position.X, pill.Position.Y] = tile1;
+                        gameBoard[pill.Position.X, pill.Position.Y + 1] = tile2;
+                        break;
+
+                    case Orientation.REVERSE_HORIZONTAL:
+                        gameBoard[pill.Position.X, pill.Position.Y] = tile2;
+                        gameBoard[pill.Position.X + 1, pill.Position.Y] = tile1;
+
+                        partnerBoard[pill.Position.X, pill.Position.Y] = Partner.RIGHT;
+                        partnerBoard[pill.Position.X + 1, pill.Position.Y] = Partner.LEFT;
+                        break;
+
+                    case Orientation.REVERSE_VERTICAL:
+                        gameBoard[pill.Position.X, pill.Position.Y] = tile1;
+                        gameBoard[pill.Position.X, pill.Position.Y - 1] = tile2;
+                        break;
+                }
+
+                RemoveTiles(gameBoard, partnerBoard);
+
+                gameBoards.TryAdd(pill, new Tuple<Tile[,], Partner[,]>(gameBoard, partnerBoard));
+            });
+
+            return gameBoards;
+        }
+
+        private static void RemoveTiles(Tile[,] gameBoard, Partner[,] partnerBoard)
+        {
+            while (true)
+            {
+                List<Position> removedTiles = new List<Position>();
+
+                CheckRemovableTilesVertically(gameBoard, removedTiles);
+                CheckRemovableTilesHorizontally(gameBoard, removedTiles);
+               
+                if (removedTiles.Count > 0)
+                {
+                    for (int row = 0; row < gameBoard.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < gameBoard.GetLength(1); col++)
+                        {
+                            Position tilePosition = new Position(row, col);
+                            if (removedTiles.Any(pos => pos.IsEqual(tilePosition)))
+                            {
+                                gameBoard[row, col] = Tile.NONE;
+
+                                switch(partnerBoard[row, col])
+                                {
+                                    case Partner.LEFT:
+                                        partnerBoard[row - 1, col] = Partner.NONE;
+                                        break;
+
+                                    case Partner.RIGHT:
+                                        partnerBoard[row + 1, col] = Partner.NONE;
+                                        break;
+                                }
+                                partnerBoard[row, col] = Partner.NONE;
+                            }
+                        }
+                    }
+
+                    DropUnsupportedPills(gameBoard, partnerBoard);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void CheckRemovableTilesVertically(Tile[,] gameBoard, List<Position> removedTiles)
+        {
+            for (int row = 0; row < gameBoard.GetLength(0); row++)
+            {
+                Color? lastColor = null;
+                int consecutiveColorsLength = 0;
+
+                for (int col = 0; col < gameBoard.GetLength(1); col++)
+                {
+                    Tile tile = gameBoard[row, col];
+                    Color? tileColor = ColorConverter.Convert(tile);
+                    if (tileColor != lastColor)
+                    {
+                        lastColor = tileColor;
+                        consecutiveColorsLength = 1;
+                    }
+                    else
+                    {
+                        if (tileColor != null)
+                        {
+                            consecutiveColorsLength++;
+
+                            if (tileColor == Color.WHITE && consecutiveColorsLength == 2)
+                            {
+                                int[] vaccinatedRows = new int[] { col, col - 1 };
+                                foreach (int vaccinatedRow in vaccinatedRows)
+                                {
+                                    for (int x = 0; x < gameBoard.GetLength(0); x++)
+                                    {
+                                        Position tilePosition = new Position(x, vaccinatedRow);
+                                        if (!removedTiles.Any(pos => pos.IsEqual(tilePosition)))
+                                        {
+                                            removedTiles.Add(tilePosition);
+                                        }
+                                    }
+                                }
+                            }
+                            else if (consecutiveColorsLength >= 4)
+                            {
+                                for (int i = 0; i < consecutiveColorsLength; i++)
+                                {
+                                    Position tilePosition = new Position(row, col - i);
+                                    if (!removedTiles.Any(pos => pos.IsEqual(tilePosition)))
+                                    {
+                                        removedTiles.Add(tilePosition);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CheckRemovableTilesHorizontally(Tile[,] gameBoard, List<Position> removedTiles)
+        {
+            for (int col = 0; col < gameBoard.GetLength(1); col++)
+            {
+                Color? lastColor = null;
+                int consecutiveColorsLength = 0;
+
+                for (int row = 0; row < gameBoard.GetLength(0); row++)
+                {
+                    Tile tile = gameBoard[row, col];
+                    Color? tileColor = ColorConverter.Convert(tile);
+                    if (tileColor != lastColor)
+                    {
+                        lastColor = tileColor;
+                        consecutiveColorsLength = 1;
+                    }
+                    else
+                    {
+                        if (tileColor != null)
+                        {
+                            consecutiveColorsLength++;
+
+                            if (tileColor == Color.WHITE && consecutiveColorsLength == 2)
+                            {
+                                int[] vaccinatedCols = new int[] { row, row - 1 };
+                                foreach(int vaccinatedCol in vaccinatedCols)
+                                {
+                                    for(int y = 0; y < gameBoard.GetLength(1); y++)
+                                    {
+                                        Position tilePosition = new Position(vaccinatedCol, y);
+                                        if (!removedTiles.Any(pos => pos.IsEqual(tilePosition)))
+                                        {
+                                            removedTiles.Add(tilePosition);
+                                        }
+                                    }
+                                }
+                            }
+                            else if (consecutiveColorsLength >= 4)
+                            {
+                                for (int i = 0; i < consecutiveColorsLength; i++)
+                                {
+                                    Position tilePosition = new Position(row - i, col);
+                                    if (!removedTiles.Any(pos => pos.IsEqual(tilePosition)))
+                                    {
+                                        removedTiles.Add(tilePosition);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void DropUnsupportedPills(Tile[,] gameBoard, Partner[,] partnerBoard)
+        {
+            for (int row = 0; row < gameBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameBoard.GetLength(1); col++)
+                {
+                    Tile tile = gameBoard[row, col];
+                    if (tile != Tile.NONE && !TileHelper.IsVirus(tile))
+                    {
+                        Partner partner = partnerBoard[row, col];
+                        if (partner != Partner.NONE)
+                        {
+                            int dropIndex = col;
+                            int partnerX = 0;
+                            if (partner == Partner.LEFT)
+                            {
+                                partnerX = row - 1;
+                            }
+                            else if (partner == Partner.RIGHT)
+                            {
+                                partnerX = row + 1;
+                            }
+
+                            while ( dropIndex > 0
+                                    && gameBoard[row, dropIndex - 1] == Tile.NONE
+                                    && gameBoard[partnerX, dropIndex - 1] == Tile.NONE)
+                            {
+                                dropIndex -= 1;
+                            }
+
+                            if (dropIndex != col)
+                            {
+                                gameBoard[row, dropIndex] = tile;
+                                gameBoard[row, col] = Tile.NONE;
+                                gameBoard[partnerX, dropIndex] = gameBoard[partnerX, col];
+                                gameBoard[partnerX, col] = Tile.NONE;
+
+                                partnerBoard[row, dropIndex] = partnerBoard[row, col];
+                                partnerBoard[row, col] = Partner.NONE;
+                                partnerBoard[partnerX, dropIndex] = partnerBoard[partnerX, col];
+                                partnerBoard[partnerX, col] = Partner.NONE;
+                            }
+                        }
+                        else
+                        {
+                            int dropIndex = col;
+                            while (dropIndex > 0 && gameBoard[row, dropIndex - 1] == Tile.NONE)
+                            {
+                                dropIndex -= 1;
+                            }
+
+                            if (dropIndex != col)
+                            {
+                                gameBoard[row, dropIndex] = tile;
+                                gameBoard[row, col] = Tile.NONE;
+                            }
+                        }
+                    } 
+                }
+            }
+        }
+        
+    }
+}
