501d4dbda89a24a09edea73973eaa4f782410ecc
shinmae09 <46877743+shinmae09@users.noreply.github.com>
2021-04-23 00:14:27 +0800

    Add files via upload

---
+++ Utility/OrientationHelper.cs
@@ -0,0 +1,14 @@
+﻿using DrMarioPlayer.Model;
+
+namespace DrMarioPlayer.Utility
+{
+    internal static class OrientationHelper
+    {
+        public static Orientation Rotate(Orientation orientation, int times)
+        {
+            int val = (int)orientation + times;
+            val = val % 4;
+            return (Orientation) val;
+        }
+    }
+}
